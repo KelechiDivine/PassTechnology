87b247f28cc6eccc9d645b9744bb6a171af16445
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-07 12:33:37 +0100

    Added some files to maven

---
+++ src/main/java/com/PassPackages/UserCanCreate.java
@@ -0,0 +1,4 @@
+package com.PassPackages;
+
+public class UserCanCreate {
+}
---
+++ src/test/java/com/PassPackages/UserCanDeleteAccountTest.java
@@ -0,0 +1,10 @@
+package com.PassPackages;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserCanDeleteAccountTest {
+	
+	@org.junit.jupiter.api.BeforeEach
+	void setUp() {
+	}
+}
