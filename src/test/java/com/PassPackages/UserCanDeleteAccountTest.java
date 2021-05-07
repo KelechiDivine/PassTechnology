c76d2056f95b9c5436a5833cf951cdafc8ec497f
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-07 16:08:11 +0100

    user can change address

--- src/test/java/com/PassPackages/UserCanDeleteAccountTest.java
+++ src/test/java/com/PassPackages/UserCanDeleteAccountTest.java
@@ -10,7 +10,7 @@ class UserCanDeleteAccountTest {
 	}
 	
 	@Test
-	 public void UserCanLogin(){
+	 public void GetUserLoginDetails(){
 		UserCanCreate userCanCreate = new UserCanCreate("okoroafor kelechi",
 				"okoroaforkelechi123@gmail.com",
 				"3, owojori street, Ogun state","08082167764", 123);
@@ -18,6 +18,25 @@ class UserCanDeleteAccountTest {
 		
 	}
 	
+	@Test
+	public  void SetANewUserName(){
+		UserCanCreate SetUserName = new UserCanCreate("okoroafor kelechi",
+				"okoroaforkelechi123@gmail.com",
+				"3, owojori street, Ogun state","08082167764", 123);
+		SetUserName.setName("Kelechi Divine");
+		Assertions.assertEquals("Kelechi Divine",SetUserName.getName());
+		
+	}
+	
+	@Test
+	public  void SetANewAddress(){
+		UserCanCreate userCanChangeAddress = new UserCanCreate("okoroafor kelechi",
+				"okoroaforkelechi123@gmail.com",
+				"3, owojori street, Ogun state","08082167764", 123);
+		userCanChangeAddress.setAddress("312 herbert macaulay, yaba, lagos");
+		Assertions.assertEquals("312 herbert macaulay, yaba, lagos", userCanChangeAddress.getAddress());
+	}
+}
 	
 
-}
+
