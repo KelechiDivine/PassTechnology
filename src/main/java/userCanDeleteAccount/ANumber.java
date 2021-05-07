1a60af9f99ef9a0b069f4df6aa609d5d28d3ac6e
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-07 15:14:23 +0100

    user can login

---
+++ src/main/java/userCanDeleteAccount/ANumber.java
@@ -0,0 +1,4 @@
+package userCanDeleteAccount;
+
+public class ANumber {
+}
--- src/test/java/com/PassPackages/UserCanDeleteAccountTest.java
+++ src/test/java/com/PassPackages/UserCanDeleteAccountTest.java
@@ -1,10 +1,20 @@
 package com.PassPackages;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class UserCanDeleteAccountTest {
 	
 	@org.junit.jupiter.api.BeforeEach
 	void setUp() {
 	}
+	
+	@Test
+	 public void UserCanLogin(){
+		UserCanCreate userCanCreate = new UserCanCreate("okoroafor kelechi",
+				"okoroaforkelechi123@gmail.com",
+				"3, owojori street, Ogun state");
+		Assertions.assertEquals("okoroafor kelechi", userCanCreate.getName());
+	}
+
 }
