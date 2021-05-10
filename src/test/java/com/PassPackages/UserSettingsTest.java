5d100466ee21ff6ff2209424a117d48c58ba8885
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-10 03:12:35 +0100

    user email and password field can't be empty

--- src/test/java/com/PassPackages/UserSettingsTest.java
+++ src/test/java/com/PassPackages/UserSettingsTest.java
@@ -1,8 +1,13 @@
 package com.PassPackages;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.EmptyStackException;
+
 
 
 class UserSettingsTest {
@@ -16,7 +21,50 @@ class UserSettingsTest {
 	}
 	
 	@Test
-	public void forgetPassword(){
+	public void forgetPassword() throws Exception {
+		FirgetPassword firgetPassword = new FirgetPassword("newPasswords");
+		
+		try {
+			String password = "oldPassword";
+			password.replace("oldPassword", "newPasswords");
+			
+			if (firgetPassword.getForgetPassword().contains("oldPassword")) {
+				System.out.println("Try To Login With Your Old Password.");
+			} else {
+				if (firgetPassword.getForgetPassword().isEmpty()) {
+					System.out.println("Password Field Can't Be Empty.. ");
+				}
+				Assertions.assertEquals("newPasswords", firgetPassword.getForgetPassword());
+				firgetPassword.setForgetPassword("newPasswords.");
+				System.out.println("Your password was updated!");
+			}
+		} catch (EmptyStackException emptyStackException) {
+			throw new Exception();
+		}
+		
+		
+	}
 	
+	@Test
+	public void ChangeEmail() throws Exception {
+		UserCanChangeEmail userCanChangeEmail = new UserCanChangeEmail("chinwenduokoroafor@gmail.com");
+		
+		try {
+			String changeEmail = "okoroaforkelechi123@gmail.com";
+			changeEmail.replace("okoroaforkelechi123@gmail.com", "chinwenduokoroafor@gmail.com");
+			
+			if (userCanChangeEmail.getChangeEmail().contains("okoroaforkelechi123@gmail.com")) {
+				System.out.println("Log on failed..");
+			} else {
+				if (userCanChangeEmail.getChangeEmail().isEmpty()) {
+					System.out.println("Email Field Can't Be Empty.. ");
+				}
+				Assertions.assertEquals("chinwenduokoroafor@gmail.com", userCanChangeEmail.getChangeEmail());
+				userCanChangeEmail.setChangeEmail("chinwenduokoroafor@gmail.com");
+				System.out.println("Email verified!");
+			}
+		} catch (Exception exception) {
+			throw new IOException();
+		}
 	}
 }
